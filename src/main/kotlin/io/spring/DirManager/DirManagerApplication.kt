1f586d52c75477b776ce4d3f9240add557588da9
rs <rs-pluss@yandex.ru>
2018-09-19 13:52:02 +0300

    Structure: Add Spring Wrapper and directory structure

---
+++ src/main/kotlin/io/spring/DirManager/DirManagerApplication.kt
@@ -0,0 +1,11 @@
+package io.spring.DirManager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DirManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<DirManagerApplication>(*args)
+}
